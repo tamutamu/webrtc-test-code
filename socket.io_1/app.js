9190d66d004f6b38b417c48d59671a7b0b76fd48
tamutamu <tamu.0.0.tamu@gmail.com>
2023-06-08 13:30:54 +0900

    第２回まで完了

--- socket.io_1/app.js
+++ socket.io_1/app.js
@@ -9,7 +9,29 @@ var server = http.createServer(function (req, res) {
 var io = socketio(server);
 
 io.sockets.on('connection', function (socket) {
+    var name;
+
     socket.on('client_to_server', function (data) {
         io.sockets.emit('server_to_client', { value: data.value });
+    });
+
+    socket.on('client_to_server_broadcast', function (data) {
+        socket.broadcast.emit('server_to_client', { value: data.value });
+    });
+
+    socket.on('client_to_server_personal', function (data) {
+        var id = socket.id;
+        name = data.value;
+        var personalMessage = "あなたは、" + name + "さんとして入室しました。"
+        io.to(id).emit('server_to_client', { value: personalMessage })
+    });
+
+    socket.on('disconnect', function () {
+        if (name == undefined) {
+            console.log("未入室のまま、どこかへ去っていきました。");
+        } else {
+            var endMessage = name + "さんが退出しました。"
+            io.sockets.emit('server_to_client', { value: endMessage });
+        }
     });
 });
